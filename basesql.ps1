552daa31cc3e632bb8c6f34ef1eacdeaa366958d
cd8 <daniel.beaty@cleardynamics.com.au>
2017-06-16 23:57:56 +1000

    Update basesql.ps1

--- basesql.ps1
+++ basesql.ps1
@@ -1,9 +1,12 @@
 #setup sql to accept sql auth
 param([string]$Password)
 
+# Folders
+New-Item -ItemType Directory c:\tmp
+
 Invoke-WebRequest https://raw.githubusercontent.com/cd8/autodeploy/master/Reset-SqlAdmin.psm1 -outfile C:\tmp\Reset-SqlAdmin.psm1
 import-module C:\tmp\Reset-SqlAdmin.psm1
 Reset-SqlAdmin -SqlServer . -Password $Password
 
 # Firewall
-netsh advfirewall firewall add rule name="tcp" dir=in action=allow protocol=TCP localport=1433
+netsh advfirewall firewall add rule name="sqlexternal" dir=in action=allow protocol=TCP localport=1433
