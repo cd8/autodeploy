4d913529d58b256e51cc4f15e07ed5a73e5801cb
cd8 <daniel.beaty@cleardynamics.com.au>
2017-06-16 20:02:23 +1000

    Update basesql.ps1

--- basesql.ps1
+++ basesql.ps1
@@ -1,5 +1,7 @@
 #setup sql to accept sql auth
 param([string]$Password)
+
+Invoke-WebRequest https://raw.githubusercontent.com/cd8/autodeploy/master/Reset-SqlAdmin.psm1 -outfile C:\tmp\Reset-SqlAdmin.psm1
 import-module C:\tmp\Reset-SqlAdmin.psm1
 Reset-SqlAdmin -SqlServer . -Password $Password
 
