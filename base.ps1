d38f0ef12928e4ba4b4fc8bbdcd06a30f4f400bb
cd8 <daniel.beaty@cleardynamics.com.au>
2017-06-23 15:57:39 +1000

    Update base.ps1

--- base.ps1
+++ base.ps1
@@ -15,6 +15,10 @@
 # Folders
 New-Item -ItemType Directory c:\tmp
 
+Set-Culture en-AU
+Set-WinHomeLocation -GeoId 12
+Set-WinSystemLocale en-AU 
+
 # Install iis
 Install-WindowsFeature -Name Web-Default-Doc,Web-Dir-Browsing,Web-Http-Errors,Web-Static-Content,Web-Http-Logging,Web-Stat-Compression,Web-Filtering,Web-Windows-Auth,Web-Net-Ext,Web-Net-Ext45,Web-Asp-Net,Web-Asp-Net45,Web-ISAPI-Ext,Web-ISAPI-Filter,Web-WebSockets,Web-Mgmt-Console,Web-Scripting-Tools,Web-Mgmt-Service,NET-Framework-Core,NET-HTTP-Activation,NET-Framework-45-Core,NET-Framework-45-ASPNET,NET-WCF-HTTP-Activation45,NET-WCF-TCP-PortSharing45,WAS-Process-Model,WAS-NET-Environment,WAS-Config-APIs
 
@@ -39,7 +43,8 @@ Start-Process c:\tmp\SQLSysClrTypes.msi -ArgumentList /passive, ADDLOCAL=ALL, /n
 Invoke-WebRequest http://go.microsoft.com/fwlink/?LinkID=239659 -outfile c:\tmp\SharedManagementObjects.msi
 Start-Process c:\tmp\SharedManagementObjects.msi -ArgumentList /passive, ADDLOCAL=ALL, /norestart -Wait
 
-
+Invoke-WebRequest https://download.microsoft.com/download/6/7/D/67D80164-7DD0-48AF-86E3-DE7A182D6815/rewrite_2.0_rtw_x64.msi -outfile c:\tmp\rewrite_2.0_rtw_x64.msi
+Start-Process c:\tmp\rewrite_2.0_rtw_x64.msi -ArgumentList /passive, ADDLOCAL=ALL, /norestart -Wait
 
 # Download music app
 # Invoke-WebRequest  https://github.com/Microsoft/dotnet-core-sample-templates/raw/master/dotnet-core-music-windows/music-app/music-store-azure-demo-pub.zip -OutFile c:\temp\musicstore.zip
